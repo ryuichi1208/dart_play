e3cfad606c986c1398d83152451b2eef11acf1ea
Ryuichi1208 <35625782+ryuichi1208@users.noreply.github.com>
2018-12-25 22:58:11 +0900

    Create get_but.dart

---
+++ src/get_but.dart
@@ -0,0 +1,11 @@
+import 'dart:html';
+ 
+void main() {
+  var cat = querySelector('#cat');
+import 'dart:html';
+ 
+void main() {
+  var cat = querySelector('#cat');
+  cat.text = "??";
+  querySelector("#catName").text = "まだ無い";
+}
